95ad452b7a99c1fdc5c0a094027b0435d5c75bbc
yegorkotseba <yegor.kotseba@gmail.com>
2018-03-13 22:35:13 +0200

    Added Square & Rectangle classes

--- sandbox/src/main/java/com/java_tr1/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/java_tr1/sandbox/MyFirstProgram.java
@@ -4,6 +4,14 @@ public class MyFirstProgram {
 
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
+
+		Square s = new Square(5);
+		System.out.println("Square area with side " + s.l + " = " + s.area());
+
+		Rectangle r = new Rectangle(4, 5);
+		System.out.println("Rectangle area with sides " + r.a + " " + r.b + " = "+ r.area());
 	}
+
+
 
 }
---
+++ sandbox/src/main/java/com/java_tr1/sandbox/Square.java
@@ -0,0 +1,14 @@
+package com.java_tr1.sandbox;
+
+public class Square {
+
+    public double l;
+
+    public Square(double len){
+        this.l = len;
+    }
+
+    public double area(){
+        return this.l * this.l;
+    }
+}
