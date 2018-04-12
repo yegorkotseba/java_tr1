9decf2dbead196b68c1aa15e71966078eed8e469
yegorkotseba <yegor.kotseba@gmail.com>
2018-04-12 19:49:05 +0300

    Added checks if page is present

--- addressbook-web-tests/src/test/java/com/java_tr1/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/com/java_tr1/addressbook/appmanager/NavigationHelper.java
@@ -10,10 +10,18 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void goToGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && driver.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))){
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void goToHomePage(){
+        if (isElementPresent(By.id("maintable"))){
+            return;
+        }
         click(By.linkText("home"));
     }
 }
