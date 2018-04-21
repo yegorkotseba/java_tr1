27cf108c2605a8ebbfe4a240b2fa6ede072cd870
yegorkotseba <yegor.kotseba@gmail.com>
2018-04-21 15:03:37 +0300

    Added GroupModification assert

--- addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupModification.java
+++ addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupModification.java
@@ -4,6 +4,7 @@ import com.java_tr1.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModification extends TestBase {
@@ -15,12 +16,17 @@ public class GroupModification extends TestBase {
             app.getGroupHelper().createGroup(new GroupData("test5", null, null));
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(0);
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test2", "test3");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() );
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
