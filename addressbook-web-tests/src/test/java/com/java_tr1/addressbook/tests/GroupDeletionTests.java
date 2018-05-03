77e80d3ed08e86309064b8cc6a3d365082c3c263
yegorkotseba <yegor.kotseba@gmail.com>
2018-05-03 15:25:18 +0300

    Improved code

--- addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupDeletionTests.java
@@ -11,24 +11,25 @@ public class GroupDeletionTests extends TestBase{
 
     @BeforeMethod
     public void ensurePreconditions(){
-        app.getNavigationHelper().goToGroupPage();
-        if (! app.getGroupHelper().isThereAGroup()){
-            app.getGroupHelper().createGroup(new GroupData("test5", null, null));
+        app.goTo().GroupPage();
+        if (app.group().list().size() == 0){
+            app.group().create(new GroupData("test5", null, null));
         }
     }
 
     @Test
     public void testGroupDeletion() throws Exception {
-        List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(0);
-        app.getGroupHelper().deleteSelectedGroups();
-        app.getGroupHelper().returnToGroupPage();
-        List<GroupData> after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after.size(), before.size() - 1);
+        List<GroupData> before = app.group().list();
+        int index = before.size() - 1;
+        app.group().delete(index);
+        List<GroupData> after = app.group().list();
+        Assert.assertEquals(after.size(), index);
 
-        before.remove(before.size() - 1);
+        before.remove(index);
         Assert.assertEquals(before , after);
     }
+
+
 }
 
 
--- addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupModification.java
+++ addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupModification.java
@@ -12,19 +12,19 @@ public class GroupModification extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions(){
-        app.getNavigationHelper().goToGroupPage();
-        if (! app.getGroupHelper().isThereAGroup()){
-            app.getGroupHelper().createGroup(new GroupData("test5", null, null));
+        app.goTo().GroupPage();
+        if (app.group().list().size() == 0){
+            app.group().create(new GroupData("test5", null, null));
         }
     }
 
     @Test
     public void testGroupModification(){
-        List<GroupData> before = app.getGroupHelper().getGroupList();
+        List<GroupData> before = app.group().list();
         int index = before.size() - 1;
         GroupData group = new GroupData(before.get(index).getId(), "test1", "test2", "test3");
-        app.getGroupHelper().modifyGroup(index, group);
-        List<GroupData> after = app.getGroupHelper().getGroupList();
+        app.group().modify(index, group);
+        List<GroupData> after = app.group().list();
         Assert.assertEquals(after.size(), before.size() );
 
         before.remove(index);
