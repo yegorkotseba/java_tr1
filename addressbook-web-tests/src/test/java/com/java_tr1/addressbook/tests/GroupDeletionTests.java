fc5f8a670c6c12257421495817bbe2e01f28b041
yegorkotseba <yegor.kotseba@gmail.com>
2018-05-03 14:56:34 +0300

    Added preconditions for delete and modify group tests

--- addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupDeletionTests.java
@@ -2,18 +2,23 @@ package com.java_tr1.addressbook.tests;
 
 import com.java_tr1.addressbook.model.GroupData;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase{
 
-    @Test
-    public void testGroupDeletion() throws Exception {
+    @BeforeMethod
+    public void ensurePreconditions(){
         app.getNavigationHelper().goToGroupPage();
         if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test5", null, null));
         }
+    }
+
+    @Test
+    public void testGroupDeletion() throws Exception {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(0);
         app.getGroupHelper().deleteSelectedGroups();
