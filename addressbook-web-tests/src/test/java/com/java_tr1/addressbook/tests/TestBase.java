b777263ac891af25afe9480914c38ecb39633dd4
yegorkotseba <yegor.kotseba@gmail.com>
2018-05-03 14:34:51 +0300

    Optimized browser launch before tests

--- addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/TestBase.java
@@ -4,19 +4,21 @@ import com.java_tr1.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-    @BeforeClass(alwaysRun = true)
+    @BeforeSuite
 
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterClass(alwaysRun = true)
+    @AfterSuite
     public void tearDown() throws Exception {
         app.stop();
     }
