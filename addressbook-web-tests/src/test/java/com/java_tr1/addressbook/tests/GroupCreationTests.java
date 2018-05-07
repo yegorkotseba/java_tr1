2f2ffe820922de4bb3d05418d9715f5475f5bab6
yegorkotseba <yegor.kotseba@gmail.com>
2018-05-07 15:57:35 +0300

    Implemented fluent interface&builder pattern

--- addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupCreationTests.java
@@ -15,7 +15,7 @@ public class GroupCreationTests extends TestBase{
 
         app.goTo().groupPage();
         List<GroupData> before = app.group().list();
-        GroupData group = new GroupData("test5", null, null);
+        GroupData group = new GroupData().withName("test5");
         app.group().create(group);
         List<GroupData> after = app.group().list();
         Assert.assertEquals(after.size(), before.size() +1);
