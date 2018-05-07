ceda41a9ac77943f855022b974bcb076e2197026
yegorkotseba <yegor.kotseba@gmail.com>
2018-05-07 13:36:14 +0300

    Optimized finding max element in collection

--- addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import com.java_tr1.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -20,15 +21,7 @@ public class GroupCreationTests extends TestBase{
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() +1);
 
-        int max = 0;
-
-        for(GroupData g : before){
-            if (g.getId() > max){
-                max = g.getId();
-            }
-        }
-
-        group.setId(max);
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
