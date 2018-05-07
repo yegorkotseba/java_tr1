afd28c324522394e4c5db5a82ad70bdb7b8be6b0
yegorkotseba <yegor.kotseba@gmail.com>
2018-05-07 13:13:16 +0300

    Reverted optimizations to stable version

--- addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/com/java_tr1/addressbook/tests/GroupDeletionTests.java
@@ -13,14 +13,14 @@ public class GroupDeletionTests extends TestBase{
     public void ensurePreconditions(){
         app.getNavigationHelper().goToGroupPage();
         if (! app.getGroupHelper().isThereAGroup()){
-            app.getGroupHelper().createGroup(new GroupData("test5", null, null));
+            app.getGroupHelper().createGroup(new GroupData("test2", null, null));
         }
     }
 
     @Test
     public void testGroupDeletion() throws Exception {
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(0);
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
